582519e4d8e15fdb57981a2c8d974d19eae16c0e
Denis Atkesone <denis.atkesone@gmail.com>
2017-11-07 05:37:46 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,7 +18,7 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
     if position_taken?(board, win_combination[0]) && ( board[win_combination[0]] == board[win_combination[1]] && board[win_combination[0]] == board[win_combination[2]] )
-      win_combination
+      return win_combination
     else
       return false
     end
