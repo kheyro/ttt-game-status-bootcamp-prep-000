415c5c9817caf4a96b46d6e3662d00699c5121a1
Denis Atkesone <denis.atkesone@gmail.com>
2017-11-07 05:37:42 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,8 +20,9 @@ def won?(board)
     if position_taken?(board, win_combination[0]) && ( board[win_combination[0]] == board[win_combination[1]] && board[win_combination[0]] == board[win_combination[2]] )
       win_combination
     else
-      false
+      return false
     end
   end
 end
+
 puts won?([" ", " ", " ", " ", " ", " ", " ", " ", " "])
